f8096ec546d5da7493154fc940643aabde7fd7f0
RIPAN-1980 <dwe.bd.07@gmail.com>
2025-12-06 11:09:25 +0600

    Text to Speak Function Completed

--- Zeni.py
+++ Zeni.py
@@ -24,3 +24,35 @@ logging.basicConfig(
     format = "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
     level= logging.INFO
 )
+
+# ACTIVATE VOICE PROPERTY (PYTTSX3)
+
+engine = pyttsx3.init("sapi5") 
+voices  = engine.getProperty("voices") 
+#voice = voices 
+#print(voice) 
+#print(voice[2].id) 
+engine.setProperty('voice' , voices[2].id)
+engine.setProperty('rate', 170) 
+
+
+# Creating Speak Function
+
+def speak(text):
+    """ this function converts text to voice
+        Args:
+            text
+        returns:
+            voice
+    """
+
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Helo, This is Ripan Deb Nath. How can I help you?") 
+
+
+# engine = pyttsx3.init("sapi5") 
+# engine.setProperty('rate', 170)
+# voices = engine.getProperty("voices")
+# engine.setProperty('voice', voices[0].id)
